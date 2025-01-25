257c8232e1ed5164f9d9aca3255a4fa5a0834928
shilcode <2850542@gmail.com>
2025-01-25 20:02:00 +0100

    .

--- portfolio-first/src/components/About.jsx
+++ portfolio-first/src/components/About.jsx
@@ -1,40 +1,46 @@
-import React from 'react'
+import React from "react";
+
 
 function About() {
   return (
-    <div name='about' className='w-full h-screen bg-[#3c1053] text-gray-300'>
-        <div className='flex flex-col justify-center items-center w-full h-full'>
-            {/* container */}
-            <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
-            <div className='sm:text-right pb-8 pl-4'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
-            </div>
-            <div></div>
-            </div>
-            <div className='max-w-[1000px] w-full px-8 grid sm:grid-cols-2 gap-8 '>
-               <div className='sm:text-right text-4xl font-bold '>
-               <p>Hi. I am Mohan 👋🏽              
-               </p>
-               <img src="https://media.giphy.com/media/5qZEZ0rTln9581K7Q5/giphy.gif" alt='Giphy' className='max-w-full' />
-               
-                
-               </div>
-               <div><p>
-                <br />
-                <br />
-                <br />
-               A motivated and enthusiastic job seeker with a strong desire to learn andgrow.
-                Eager to contribute to a dynamic team and gain practical experience. 
-                Possesses excellent communication and interpersonal skills, 
-                focusing ondelivering exceptional customer service. 
-                Committed to professionalism,reliability, and continuous self-improvement. 
-                Seeking an entry-levelopportunity to kickstart a successful career.
-                </p></div>
-            </div>
-          
+    <div name="about" className="w-full h-screen bg-[#3c1053] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        {/* container */}
+        <div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
         </div>
+        <div className="max-w-[1000px] w-full px-8 grid sm:grid-cols-2 gap-8 ">
+          <div className="sm:text-right text-4xl font-bold ">
+            <p>Hi. I am Mohan 👋🏽</p>
+         
+            <img
+              src="https://media.giphy.com/media/5qZEZ0rTln9581K7Q5/giphy.gif"
+              alt="Giphy"
+              className="max-w-full"
+            />
+          </div>
+          <div>
+            <p>
+              <br />
+              <br />
+              <br />A motivated and enthusiastic job seeker with a strong desire
+              to learn and grow. Eager to contribute to a dynamic team and gain
+              practical experience. Possesses excellent communication and
+              interpersonal skills, focusing on delivering exceptional customer
+              service. Committed to professionalism,reliability, and continuous
+              self-improvement. Seeking an entry-level opportunity to kickstart
+              a successful career.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- portfolio-first/src/components/Skills.jsx
+++ portfolio-first/src/components/Skills.jsx
@@ -1,82 +1,87 @@
-import React from 'react'
-
-import JS from '../assets/javascript.png'
-import playwright from '../assets/playwright.png'
-
-
-
+import React from "react";
+import JS from "../assets/javascript.png";
 
 export const Skills = () => {
   return (
-    <div name='skills' className='bg-[#3c1053] text-gray-300 w-full h-screen'>
-        {/* container */}
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
-                <p className='py-4'>These are some of my skills</p>
-            </div>
-
-            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-                {/* card component */}
-                <div className='shadow-md shadow-black-900 hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src={JS} alt="html icon" />
-                    <p className='my-4'>
-                    JavaScript - Fundamentals
-                    </p>
-                </div>
-
-                {/* end of card component */}
-                <div className='shadow-md shadow-black-900 hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" alt="html icon" />
-                    <p className='my-4' >
-
-                       Google Suite
-                    </p>
-                </div>
-                <div className='shadow-md shadow-black-900 hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Microsoft_Office_logo_%282013%E2%80%932019%29.svg/1728px-Microsoft_Office_logo_%282013%E2%80%932019%29.svg.png" alt="postman" />
-                    <p className='my-4 '>
-                       Microsoft Office Suite
-                    </p>
-                </div>
-                <div className='shadow-md shadow-black-900 hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/2560px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png" alt="Jenkins" />
-                    <p className='my-4'>
-                        English - Professional
-                    </p>
-                </div>
-                <div className='shadow-md shadow-black-900 hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src="https://cdn.iconscout.com/icon/free/png-256/free-jira-282222.png?f=webp" alt="html icon" />
-                    <p className='my-4'>
-                        JIRA | Test Management Tool
-                    </p>
-                </div>
-
-                <div className='shadow-md shadow-black-900 hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src="https://w7.pngwing.com/pngs/882/180/png-transparent-test-automation-software-testing-manual-testing-selenium-others-angle-trademark-logo-thumbnail.png" alt="html icon" />
-                    <p className='my-4'>
-                    Functional Testing
-                    </p>
-                </div>
-
-                <div className='shadow-md shadow-black-900 hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src="https://cdn.dribbble.com/users/1464433/screenshots/4128004/animated_svg_logo_-_typing_animation.gif" alt="html icon" />
-                    <p className='my-4'>
-                        Typing  45 wpm
-                    </p>
-                </div>
-                
-                <div className='shadow-md shadow-black-900 hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' src="https://community.infoblox.com/t5/image/serverpage/image-id/2195iA290BF7E3BA6064D/image-size/large/is-moderation-mode/true?v=v2&px=999" alt="html icon" />
-                    <p className='my-4'>
-                        BASH
-                    </p>
-                </div>
-
-            </div>
+    <div name="skills" className="bg-[#3c1053] text-gray-300 w-full h-screen">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Skills
+          </p>
+          <p className="py-4">These are some of my skills</p>
         </div>
 
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          {/* card component */}
+          <div className="shadow-md shadow-black-900 hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JS} alt="html icon" />
+            <p className="my-4">JavaScript - Fundamentals</p>
+          </div>
 
+          {/* end of card component */}
+          <div className="shadow-md shadow-black-900 hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
+              alt="html icon"
+            />
+            <p className="my-4">Google Suite</p>
+          </div>
+          <div className="shadow-md shadow-black-900 hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Microsoft_Office_logo_%282013%E2%80%932019%29.svg/1728px-Microsoft_Office_logo_%282013%E2%80%932019%29.svg.png"
+              alt="postman"
+            />
+            <p className="my-4 ">Microsoft Office Suite</p>
+          </div>
+          <div className="shadow-md shadow-black-900 hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/2560px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png"
+              alt="Jenkins"
+            />
+            <p className="my-4">English - Professional</p>
+          </div>
+          <div className="shadow-md shadow-black-900 hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="https://cdn.iconscout.com/icon/free/png-256/free-jira-282222.png?f=webp"
+              alt="html icon"
+            />
+            <p className="my-4">JIRA | Test Management Tool</p>
+          </div>
+
+          <div className="shadow-md shadow-black-900 hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="https://w7.pngwing.com/pngs/882/180/png-transparent-test-automation-software-testing-manual-testing-selenium-others-angle-trademark-logo-thumbnail.png"
+              alt="html icon"
+            />
+            <p className="my-4">Functional Testing</p>
+          </div>
+
+          <div className="shadow-md shadow-black-900 hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="https://cdn.dribbble.com/users/1464433/screenshots/4128004/animated_svg_logo_-_typing_animation.gif"
+              alt="html icon"
+            />
+            <p className="my-4">Typing 45 wpm</p>
+          </div>
+
+          <div className="shadow-md shadow-black-900 hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src="https://community.infoblox.com/t5/image/serverpage/image-id/2195iA290BF7E3BA6064D/image-size/large/is-moderation-mode/true?v=v2&px=999"
+              alt="html icon"
+            />
+            <p className="my-4">BASH</p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
