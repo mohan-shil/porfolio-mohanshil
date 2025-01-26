ce3e62e29603eaaa12a81272961a42d37eeb1cd4
Mohan Shil <mohanshil05@gmail.com>
2025-01-26 01:34:53 +0100

    fixing icons

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,7 +1,21 @@
+import {
+  FaJsSquare,
+  FaNodeJs,
+  FaGit,
+  FaJenkins,
+  FaDocker,
+  FaDev,
+  FaJira,
+  FaDatabase,
+} from "react-icons/fa";
+import { SiTypescript, SiPostman, SiMeteor } from "react-icons/si";
+
 const TechStack = () => {
   return (
     <div className="bg-gray-900 text-white p-4 sm:p-16" id="technology-stack">
-      <h2 className="text-3xl font-bold mb-4 text-yellow-500">Technology Stack</h2>
+      <h2 className="text-3xl font-bold mb-4 text-yellow-500">
+        Technology Stack
+      </h2>
       <div className="text-sm sm:text-lg leading-relaxed">
         <table className="table-auto w-full">
           <thead>
@@ -12,8 +26,14 @@ const TechStack = () => {
           </thead>
           <tbody>
             <tr>
-              <td className="border px-2 sm:px-4 py-2">Programming Languages</td>
-              <td className="border px-2 sm:px-4 py-2">JavaScript/TypeScript</td>
+              <td className="border px-2 sm:px-4 py-2">
+                Programming Languages
+              </td>
+              <td className="border px-2 sm:px-4 py-2 flex items-center space-x-2">
+                <FaJsSquare size={24} />
+                <SiTypescript size={24} />
+                <span>JavaScript/TypeScript</span>
+              </td>
             </tr>
             <tr>
               <td className="border px-2 sm:px-4 py-2">Testing</td>
@@ -30,15 +50,42 @@ const TechStack = () => {
               <td className="border px-2 sm:px-4 py-2">Tools</td>
               <td className="border px-2 sm:px-4 py-2">
                 <ul className="list-disc list-inside">
-                  <li>GIT</li>
-                  <li>JENKINS</li>
-                  <li>BASH</li>
-                  <li>POSTMAN</li>
-                  <li>DOCKER</li>
-                  <li>AZURE</li>
-                  <li>DEVOPS</li>
-                  <li>JIRA</li>
-                  <li>JMeter</li>
+                  <li className="flex items-center space-x-2">
+                    <FaGit size={24} />
+                    <span>Git</span>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <FaJenkins size={24} />
+                    <span>Jenkins</span>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <FaDatabase size={24} />
+                    <span>Bash</span>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <SiPostman size={24} />
+                    <span>Postman</span>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <FaDocker size={24} />
+                    <span>Docker</span>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <FaNodeJs size={24} />
+                    <span>Azure</span>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <FaDev size={24} />
+                    <span>DevOps</span>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <FaJira size={24} />
+                    <span>Jira</span>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <SiMeteor size={24} />
+                    <span>JMeter</span>
+                  </li>
                 </ul>
               </td>
             </tr>
