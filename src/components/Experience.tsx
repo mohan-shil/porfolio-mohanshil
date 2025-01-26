5db79c526beb935a97001c2f2eabb16420183154
Mohan Shil <mohanshil05@gmail.com>
2025-01-26 01:13:33 +0100

    update experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,42 +1,42 @@
 const Experience = () => {
   return (
     <div id="experience" className="bg-gray-900 text-white p-4 sm:p-16">
-      <h2 className="text-3xl font-bold mb-4">Experience 🔬</h2>
+      <h2 className="text-3xl font-bold mb-4 text-yellow-500">Experience 🔬</h2>
       <div className="text-sm sm:text-lg leading-relaxed">
-        <h3 className="text-2xl font-semibold mb-2">
-          Eastern Group, Bangladesh— QA Engineer
-        </h3>
-        <p className="mb-4">JULY 2023 - AUGUST 2024</p>
-        <p className="mb-4">
-          SCRUM team collaboration, Maintaining AQA structure, maintaining large
-          scaled automation repository, generating E2E tests proactively,
-          running pipelines regularly, pair programming, knowledge transfer,
-          evaluating a11y tests periodically, gathering/discussing valuable test
-          artifact(s) are some of my key responsibilities in this position.
-        </p>
+        <table className="table-auto w-full">
+          <thead>
+            <tr>
+              <th className="px-2 sm:px-4 py-2">Position</th>
+              <th className="px-2 sm:px-4 py-2">Duration</th>
+              <th className="px-2 sm:px-4 py-2">Responsibilities</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border px-2 sm:px-4 py-2">Eastern Group, Bangladesh— QA Engineer</td>
+              <td className="border px-2 sm:px-4 py-2">JULY 2023 - AUGUST 2024</td>
+              <td className="border px-2 sm:px-4 py-2">
+                SCRUM team collaboration, Maintaining AQA structure, maintaining large scaled automation repository, generating E2E tests proactively, running pipelines regularly, pair programming, knowledge transfer, evaluating a11y tests periodically, gathering/discussing valuable test artifact(s) are some of my key responsibilities in this position.
+              </td>
+            </tr>
+            <tr>
+              <td className="border px-2 sm:px-4 py-2">Genex, Bangladesh— QA Engineer</td>
+              <td className="border px-2 sm:px-4 py-2">FEBRUARY 2021 - JUNE 2023</td>
+              <td className="border px-2 sm:px-4 py-2">
+                Security procedures, Policy adherence, Test design, Mentoring, Task completion, Best practices, Line management, Staff evaluation, Client communication, duties & Time monitoring were some of my key responsibilities in this position.
+              </td>
+            </tr>
+            <tr>
+              <td className="border px-2 sm:px-4 py-2">North South, Bangladesh—QA Engineer</td>
+              <td className="border px-2 sm:px-4 py-2">APRIL 2019 - JANUARY 2021</td>
+              <td className="border px-2 sm:px-4 py-2">
+                Project management, Workflow integration, Leadership, Team management, Communication, Feedback, Performance evaluation were some of my key responsibilities in this position.
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-        <h3 className="text-2xl font-semibold mb-2">
-          Genex, Bangladesh— QA Engineer
-        </h3>
-        <p className="mb-4">FEBRUARY 2021 - JUNE 2023</p>
-        <p className="mb-4">
-          Security procedures, Policy adherence, Test design, Mentoring, Task
-          completion, Best practices, Line management, Staff evaluation, Client
-          communication, duties & Time monitoring were some of my key
-          responsibilities in this position.
-        </p>
-
-        <h3 className="text-2xl font-semibold mb-2">
-          North South, Bangladesh—QA Engineer
-        </h3>
-        <p className="mb-4">APRIL 2019 - JANUARY 2021</p>
-        <p className="mb-4">
-          Project management, Workflow integration, Leadership, Team management,
-          Communication, Feedback, Performance evaluation were some of my key
-          responsibilities in this position.
-        </p>
-
-        <h2 className="text-3xl font-bold mb-4">Education 📚</h2>
+        <h2 className="text-3xl font-bold mb-4 mt-8">Education 📚</h2>
         <h3 className="text-2xl font-semibold mb-2">
           WSB University, Poland— Management Engineering, Bachelors — Final Year
         </h3>
