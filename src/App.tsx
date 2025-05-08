50aec00a3ed23476e1903684a8d042b5fddacee8
mohanshil <mohanshil05@gmail.com>
2025-05-08 18:54:51 +0200

    removed specialized section

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
-import Specialization from "./components/Specialization";
+
 import TechStack from "./components/TechStack";
 import Projects from "./components/Projects";
 
@@ -14,7 +14,6 @@ function App() {
       <TechStack />
       <Projects />
       <Experience />
-      <Specialization />
       <Contact />
     </div>
   );
