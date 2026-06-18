651e404b16b74336c9b7396a859bbc1d4d023b60
Mohan Shil <121026741+mohan-shil@users.noreply.github.com>
2026-06-18 22:00:45 +0000

    Techstack section hided and relocate Techstack to hero section

--- Programming/streamline-landing/app/page.tsx
+++ Programming/streamline-landing/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
-      <TechStack />
+      {/* <TechStack /> */}
       <SkillBars />
       <QAConsulting />
       <Projects />
